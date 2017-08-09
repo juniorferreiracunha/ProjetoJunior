217726aaa2bf428a573a809dd2c2d1eb98283142
Junior <Junior@Junior-PC>
2017-08-09 12:16:04 -0300

    Commit

---
+++ ProjetoJunior/src/main/java/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String home(ModelMap model) {
+	return "home";
+	}	
+
+}
